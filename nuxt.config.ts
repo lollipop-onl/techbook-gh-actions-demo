57415da8b6f1cf94462502b652db1676c00c6c41
lollipop-onl <lollipop@simoochee.net>
2020-01-10 15:43:46 +0900

    🔧 Nuxt設定を追加

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+import { Configuration } from '@nuxt/types'
+
+const config: Configuration = {
+  // ビルドモード
+  mode: 'spa',
+  // ソースディレクトリパス
+  srcDir: 'src',
+  // headタグ内の項目
+  head: {
+    title: 'デモプロジェクト',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  },
+  // グローバルCSSファイル
+  css: [],
+  // vue-router
+  router: {
+    mode: 'hash'
+  },
+  // ビルド時のみ使用するモジュール
+  buildModules: [
+    '@nuxt/typescript-build'
+  ],
+  // Nuxt Module
+  modules: [
+    '@nuxtjs/style-resources'
+  ],
+  // 全体で使用するCSSファイルを指定
+  styleResources: {
+    scss: []
+  }
+}
+
+module.exports = config
