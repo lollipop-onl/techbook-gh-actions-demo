4665d127ac2149e94c1c68e4a4fcad38cb071707
lollipop-onl <lollipop@simoochee.net>
2020-01-26 14:21:53 +0900

    💡 テストシナリオにコメントを追加

--- e2e/e2e.ts
+++ e2e/e2e.ts
@@ -1,9 +1,14 @@
 import { Selector, ClientFunction } from 'testcafe'
 
+/** 名前入力欄の要素 */
 const nameInput = Selector('.form > .form__input')
+/** 送信ボタンの要素 */
 const submitButton = Selector('.form > .form__submit')
-const userNameElement = Selector('.greeting > .greeting__name')
-const testUserName = '山田太郎'
+/** 名前が表示されている要素 */
+const nameElement = Selector('.greeting > .greeting__name')
+/** テストのユーザー名 */
+const testName = '山田太郎'
+/** NuxtのページURLを取得する */
 const getPagePath = ClientFunction(() => {
   const { hash } = window.location
   const href = hash.replace(/^#/, '')
@@ -11,30 +16,39 @@ const getPagePath = ClientFunction(() => {
   return href.replace(/^(.*)\?.*/, '$1')
 })
 
+// シナリオ全体の見出しと開始時点のページURL
 fixture `デモプロジェクト E2Eテスト`
   .page `http://localhost:8080`
 
+// テストシナリオ
 test('フォーム送信後、/greetingへ遷移する', async (t) => {
   await t
-    .typeText(nameInput, testUserName)
+    // 名前入力欄に名前を入力
+    .typeText(nameInput, testName)
+    // 送信ボタン押下
     .click(submitButton)
-    .expect(getPagePath())
-    .eql('/greeting')
+    // ページURLがが /greeting かどうか
+    .expect(getPagePath()).eql('/greeting')
 })
 
+// テストシナリオ
 test('フォーム送信後、画面上に入力値が表示される', async (t) => {
   await t
-    .typeText(nameInput, testUserName)
+    // 名前入力欄に名前を入力
+    .typeText(nameInput, testName)
+    // 送信ボタン押下
     .click(submitButton)
-    .expect(userNameElement.textContent)
-    .contains(testUserName)
+    // 名前表示に入力した指名が含まれるかどうか
+    .expect(nameElement.textContent).contains(testName)
 })
 
+// テストシナリオ
 test('挨拶画面へ直接アクセスするとフォームページへリダイレクトする', async (t) => {
   await t
+    // #/greeting へ遷移
     .navigateTo('#/greeting')
-    .expect(nameInput.exists)
-    .ok()
-    .expect(getPagePath())
-    .eql('/')
+    // 名前入力欄が表示されるまで待機
+    .expect(nameInput.exists).ok()
+    // ページURLが / かどうか
+    .expect(getPagePath()).eql('/')
 })
