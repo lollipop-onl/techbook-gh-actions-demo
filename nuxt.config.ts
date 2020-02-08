03de3623e993432506b9b6ef82448e28ab2c8a57
lollipop-onl <lollipop@simoochee.net>
2020-02-08 13:38:59 +0900

    🐛 Fixed typo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ const config: Configuration = {
     scss: [
       '@/assets/styles/vars.scss',
       '@/assets/styles/media.scss',
-      '@/assets/styles/mixinsz1.scss',
+      '@/assets/styles/mixins.scss',
       '@/assets/styles/components/*.scss',
     ],
   },
